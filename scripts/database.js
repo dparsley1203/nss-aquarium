9e4f4a83baba3b2309383375812ab6e7317374ab
dparsley1203 <dparsley1203@gmail.com>
2021-05-27 09:34:59 -0500

    original martins aquarium-first try

---
+++ scripts/database.js
@@ -0,0 +1,11 @@
+const database = {
+  fish: [
+      {
+          species: "Goldfish",
+          length: 3,
+          name: "Ted",
+          location: "Tennessee",
+          diet: "Alge",
+      }
+  ]
+}
